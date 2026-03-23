19f75604756974052e208659d2a94a71b3e74a24
NoahIsGreatest <makeistrue@gmail.com>
2026-03-23 03:46:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+
+let equation = ""
+const Btn0 = document.getElementById("Btn0")
+const calculateButton = document.getElementById('CalculateBtn')
+
+const symbols = document.querySelectorAll('.calcBtn')
+if (symbols) {
+    symbols.forEach(element => {
+        element.addEventListener('click', () => {
+            equation = equation + `${element.innerHTML}`
+            updateDisplay('update')
+        })
+    });
+}
+
+function updateDisplay(to) {
+    const display = document.getElementById('displayer')
+    if (display) {
+        if (to == "calculate") {
+            let safeEquation = equation.replace(/÷/g, '/');
+            safeEquation = safeEquation.replace(/X/g, '*');
+            
+            let output;
+            try {
+
+                output = eval(safeEquation)
+                equation = ""
+                safeEquation = ""
+
+            } catch {
+                output = "Error"
+                equation = ""
+                safeEquation = ""
+            }
+            display.innerHTML = `${output}`
+
+
+        } else {
+            display.innerHTML = `${equation}`
+        }
+
+    }
+}
+
+
+
+
+if (calculateButton) {
+    calculateButton.addEventListener('click', () => {
+        updateDisplay('calculate')
+    })
+}
+if (Btn0) {
+    Btn0.addEventListener("click", () => {
+        equation = equation + `${Btn0.innerHTML}`
+        updateDisplay('update')
+         console.log(`Button Number 0 was clicked, current equation: ${equation}`)
+
+    })
+}
+for (let i = 1; i < 10; i++) {
+    const button = document.getElementById(`Btn${i}`)
+    if (button) {
+        button.addEventListener("click", () => {
+            equation = equation + `${button.innerHTML}`
+            updateDisplay('update')
+            console.log(`Button Number ${button.innerHTML} was clicked, current equation: ${equation}`)
+        })
+    }
+}
